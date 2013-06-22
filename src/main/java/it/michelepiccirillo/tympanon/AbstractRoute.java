8127565a8199ba6a6730339dc998c965774bd642
Michele Piccirillo <michele.piccirillo@gmail.com>
2013-06-22 10:16:18 +0200

    Project conversion to Maven

--- src/main/java/it/michelepiccirillo/tympanon/AbstractRoute.java
+++ src/main/java/it/michelepiccirillo/tympanon/AbstractRoute.java
@@ -3,37 +3,6 @@ package it.michelepiccirillo.tympanon;
 import it.michelepiccirillo.tympanon.HttpResponse.Status;
 
 public class AbstractRoute implements Route {
-
-	@Override
-	public void service(HttpRequest req, HttpResponse res) throws Exception {
-		switch(req.getMethod()) {
-		case OPTIONS:
-			options(req, res);
-			break;
-		case GET:
-			get(req, res);
-			break;
-		case HEAD:
-			head(req,res);
-			break;
-		case POST:
-			post(req, res);
-			break;
-		case PUT:
-			put(req,res);
-			break;
-		case DELETE:
-			delete(req,res);
-			break;
-		case TRACE:
-			trace(req,res);
-			break;
-		case CONNECT:
-			connect(req,res);
-			break;
-			
-		}
-	}
 	
 	protected void options(HttpRequest req, HttpResponse res) throws Exception {
 		res.setStatus(Status.METHOD_NOT_ALLOWED);
@@ -65,5 +34,35 @@ public class AbstractRoute implements Route {
 	
 	protected void connect(HttpRequest req, HttpResponse res) throws Exception {
 		res.setStatus(Status.METHOD_NOT_ALLOWED);
+	}
+
+	public void service(HttpRequest req, HttpResponse res) throws Exception {
+		switch(req.getMethod()) {
+		case OPTIONS:
+			options(req, res);
+			break;
+		case GET:
+			get(req, res);
+			break;
+		case HEAD:
+			head(req,res);
+			break;
+		case POST:
+			post(req, res);
+			break;
+		case PUT:
+			put(req,res);
+			break;
+		case DELETE:
+			delete(req,res);
+			break;
+		case TRACE:
+			trace(req,res);
+			break;
+		case CONNECT:
+			connect(req,res);
+			break;
+			
+		}	
 	}
 }
